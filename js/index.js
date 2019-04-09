130e52fa01f7f46efbe080dae05d79abe23ace41
EoPlex <you@example.com>
2019-04-09 15:50:10 -0700

    forgot to commit a bunch sorry

--- js/index.js
+++ js/index.js
@@ -1,12 +1,72 @@
 // Your code goes here
 
-const navColor = document.querySelectorAll('header.nav')
+
+
+let signBtns = document.querySelector(".content-pick");
+
+signBtns.addEventListener('click', function(){
+    if (event.target.className === "btn") {
+
+        event.target.style.backgroundColor = "blue";
+        event.target.color = "white";
+
+    }
+});
+
+
+const navColor = document.querySelector('header .nav');
 
 navColor.addEventListener("mouseover", function(event) {
-    event.target.style.color = "blue";
+    event.target.style.color = "green";
 
     setTimeout(function() {
         event.target.style.color = "";
     }, 500);
 }, false);
 
+
+
+const headerChange = document.querySelectorAll('.text-content');
+
+    headerChange.forEach(header => {
+    header.addEventListener("wheel", function(event) {
+    event.target.style.color = "blue";
+});
+});
+
+
+
+document.addEventListener("copy", function(event) {
+    event.preventDefault();
+    alert('DO NOT COPY!!!')
+});
+
+let animateHeader = document.querySelector(".logo-heading");
+animateHeader.addEventListener("click", function(event) {
+    TweenLite.to(animateHeader, 3, {rotation: 720});
+})
+
+
+/* document.addEventListener("select", function(event) {
+    event.target.style.backgroundColor = "yellow";
+});
+*/
+
+// const colorLogo = document.querySelector(".logo-heading");
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode == 81) {
+        document.querySelector(".logo-heading").style.color = "red";
+    }
+})
+
+let resizedColor = document.querySelector(".intro h2");
+window.addEventListener("resize", function(event) {
+    resizedColor.style.backgroundColor = "gold";
+})
+
+
+let colorifyWord = document.querySelector(".text-content h2");
+colorifyWord.addEventListener("mousedown", function(event) {
+    event.target.style.color = "green";
+});
